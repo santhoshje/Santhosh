c4f5035bfd34f1ff552186311304ce2e66c92f07
santhoshje <144518685+santhoshje@users.noreply.github.com>
2023-09-25 13:39:01 +0530

    main.py challenge 1.2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def isleapyear(year):
-  if (year % 4 == 0) and (year % 100 == 0) or year % 400 == 0:
+  if (year % 4 == 0) and (year % 100!= 0) or year % 400 == 0:
     return true
   else:
     return False
